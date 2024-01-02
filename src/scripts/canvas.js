0f81b73a59377b9713759cae27fb919917564d34
farhanzip <farhanaji317@gmail.com>
2024-01-02 15:42:18 +0700

    feat: color picker

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,29 +1,48 @@
 export const canvas = (p) => {
-  const pixelSize = 16;
   const rowLength = 30;
   const colLength = 30;
   const canvasPx = 512;
   let backgroundLayer, drawingLayer;
+  let div;
+  let colorPicker;
+  let currentTool = "Pen";
 
   p.setup = () => {
     p.createCanvas(canvasPx, canvasPx);
+    div = p.createDiv(`<div id="btn-container"></div>`);
+    
     backgroundLayer = p.createGraphics(canvasPx, canvasPx);
     drawingLayer = p.createGraphics(canvasPx, canvasPx);
+
     displayBackground(backgroundLayer);
     displayGrid(drawingLayer);
+
+    createTextBox(currentTool);
+    createButtonWithAction("Clear", clearDrawing);
+    createButtonWithAction("Pen", () => setTool("Pen"));
+    createButtonWithAction("Eraser", () => setTool("Eraser"));
+    createColorPickers();
   };
 
   p.draw = () => {
-    p.image(backgroundLayer, 0, 0); // Draw backgroundLayer
-    p.image(drawingLayer, 0, 0); // Draw drawingLayer
+    p.image(backgroundLayer, 0, 0);
+    p.image(drawingLayer, 0, 0);
 
     if (p.mouseIsPressed) {
-      var color = p.mouseButton === p.LEFT ? "black": "transparent";
+      let color = currentTool === "Pen" ? colorPicker.color().toString() : "transparent";
       displayPixel(p.mouseX, p.mouseY, color);
     }
+    
+  };
+
+  const calculatePixelSize = () => {
+    let maxRowsCols = Math.max(rowLength, colLength);
+    return Math.floor(canvasPx / maxRowsCols);
   };
 
   const displayGrid = (layer) => {
+    let pixelSize = calculatePixelSize();
+
     for (let row = 0; row < rowLength; row++) {
       for (let col = 0; col < colLength; col++) {
         let x = col * pixelSize;
@@ -35,7 +54,29 @@ export const canvas = (p) => {
     }
   };
 
+  const displayPixel = (x, y, color) => {
+    let pixelSize = calculatePixelSize();
+
+    let col = Math.floor(x / pixelSize);
+    let row = Math.floor(y / pixelSize);
+    if (col >= colLength || row >= rowLength) return;
+
+    // Determine the cell's left upper-corner x and y coordinates
+    var pixelX = col * pixelSize;
+    var pixelY = row * pixelSize;
+
+    drawingLayer.noStroke();
+
+    if (color === "transparent") {
+      drawingLayer.image(backgroundLayer, pixelX, pixelY, pixelSize, pixelSize, pixelX, pixelY, pixelSize, pixelSize);
+    } else {
+        drawingLayer.fill(color);
+        drawingLayer.rect(pixelX, pixelY, pixelSize, pixelSize);
+    }
+  };
+
   const displayBackground = (layer) => {
+    let pixelSize = calculatePixelSize();
     layer.background(255);
     layer.strokeWeight(0);
 
@@ -71,26 +112,32 @@ export const canvas = (p) => {
       }
     }
   };
-  
-  
-  
 
-  const displayPixel = (x, y, color) => {
-    let col = Math.floor(x / pixelSize);
-    let row = Math.floor(y / pixelSize);
-    if (col >= colLength || row >= rowLength) return;
-
-    // Determine the cell's left upper-corner x and y coordinates
-    var pixelX = col * pixelSize;
-    var pixelY = row * pixelSize;
-
-    drawingLayer.noStroke();
-    // Check if the color is 'transparent'
-    if (color !== 'transparent') {
-      drawingLayer.fill(color);
-      drawingLayer.rect(pixelX, pixelY, pixelSize, pixelSize);
-    } else {
-      drawingLayer.clear(pixelX, pixelY, pixelSize, pixelSize);
-    }
+  const createButtonWithAction = (title, buttonPressedAction) => {
+      let button = p.createButton(title);
+      button.parent('btn-container');
+      button.id(title);
+      button.mousePressed(buttonPressedAction);
   };
+
+  const clearDrawing = () => {
+      drawingLayer.clear();
+      displayGrid(drawingLayer);
+  };
+
+  const setTool = (label) => {
+    currentTool=label;
+  }
+
+  const createColorPickers = () => {
+    colorPicker = p.createColorPicker('#000000');
+    colorPicker.parent('btn-container');
+  }
+
+  const createTextBox = (title) => {
+    let button = p.createButton(title);
+    button.parent('btn-container');
+  }
+  
+
 };
