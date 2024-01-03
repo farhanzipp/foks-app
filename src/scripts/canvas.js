e1f12daee3f0986b13f909964a6489cbdcda1e2a
farhanzip <farhanaji317@gmail.com>
2024-01-03 16:02:15 +0700

    feat: add slider to adjust pen size

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,11 +1,19 @@
 export const canvas = (p) => {
-  const rowLength = 20;
-  const colLength = 20;
-  const canvasPx = 512;
+  let rowLength = 20;
+  let colLength = 20;
+  const canvasPx = 500;
   let backgroundLayer, drawingLayer;
   let div;
+
   let colorPicker;
-  let currentTool = "Pen";
+  let selectBg;
+  let sliderPenSize;
+
+  let inputPixelLength = 30;
+  let currentTool = "pen";
+  let currentBackground = "background1";
+  let penSize = 1;
+  let drawBackground = true;
 
   p.setup = () => {
     p.createCanvas(canvasPx, canvasPx);
@@ -13,26 +21,37 @@ export const canvas = (p) => {
     
     backgroundLayer = p.createGraphics(canvasPx, canvasPx);
     drawingLayer = p.createGraphics(canvasPx, canvasPx);
-
-    displayBackground3(backgroundLayer);
+    
     displayGrid(drawingLayer);
+    showBackground(currentBackground);
 
     createButtonWithAction("Clear", clearDrawing);
-    createButtonWithAction("Pen", () => setTool("Pen"));
-    createButtonWithAction("Eraser", () => setTool("Eraser"));
+    createButtonWithAction("Pen", () => setTool("pen"));
+    createButtonWithAction("Eraser", () => setTool("eraser"));
     createColorPickers();
     createSelectBg();
+    createSliderPenSize();
+    createButtonWithAction("Download", downloadImg);
+    createButtonWithAction("bg clear", toggleBackground);
+    // createInputPixelLength();
   };
 
   p.draw = () => {
-    p.image(backgroundLayer, 0, 0);
-    p.image(drawingLayer, 0, 0);
-    
-    if (p.mouseIsPressed) {
-      let color = currentTool === "Pen" ? colorPicker.color().toString() : "transparent";
-      displayPixel(p.mouseX, p.mouseY, color);
+    if (drawBackground) {
+      p.image(backgroundLayer, 0, 0);
     }
-    
+    p.image(drawingLayer, 0, 0);
+
+    if (p.mouseIsPressed) {
+      let pixelColor;
+      if(currentTool === "pen") {
+        pixelColor = colorPicker.color();
+      } else if (currentTool === "eraser") {
+        pixelColor = p.color(255, 255, 255);
+      }
+      displayPixel(p.mouseX, p.mouseY, pixelColor);
+    }
+
   };
 
   const calculatePixelSize = () => {
@@ -46,105 +65,110 @@ export const canvas = (p) => {
       for (let col = 0; col < colLength; col++) {
         let x = col * pixelSize;
         let y = row * pixelSize;
-        layer.fill(255, 0);
+        layer.noFill();
         layer.noStroke();
         layer.rect(x, y, pixelSize, pixelSize);
       }
     }
   };
 
-  const displayPixel = (x, y, color) => {
+  const displayPixel = (x, y, pixelColor) => {
     let pixelSize = calculatePixelSize();
+    pixelSize *= penSize;
+
     let col = Math.floor(x / pixelSize);
     let row = Math.floor(y / pixelSize);
-    if (col >= colLength || row >= rowLength) return;
-
+    // if (col >= colLength || row >= rowLength) return;
+  
     // Determine the cell's left upper-corner x and y coordinates
     var pixelX = col * pixelSize;
     var pixelY = row * pixelSize;
-
+  
     drawingLayer.noStroke();
-
-    if (color === "transparent") {
-      drawingLayer.image(backgroundLayer, pixelX, pixelY, pixelSize, pixelSize, pixelX, pixelY, pixelSize, pixelSize);
-    } else {
-        drawingLayer.fill(color);
-        drawingLayer.rect(pixelX, pixelY, pixelSize, pixelSize);
+    if (currentTool === "eraser") {
+      drawingLayer.erase();
     }
+  
+    drawingLayer.fill(pixelColor);
+    drawingLayer.rect(pixelX, pixelY, pixelSize, pixelSize);
+  
+    // Disable eraser effect
+    drawingLayer.noErase();
   };
-
-  const displayBackground = (layer) => {
+  
+  const showBackground = (selectedBg) => {
     let pixelSize = calculatePixelSize();
-    layer.background(255);
+    const layer = backgroundLayer;
     layer.strokeWeight(0);
 
-    for (let i = 0; i < layer.width; i += pixelSize) {
-      for (let j = 0; j < layer.height; j += pixelSize) {
-        if ((i / pixelSize + j / pixelSize) % 2 === 0) {
-          layer.fill(200); // Gray
-        } else {
-          layer.fill(255); // White
+    if (selectedBg === "background1") {
+      for (let i = 0; i < layer.width; i += pixelSize) {
+        for (let j = 0; j < layer.height; j += pixelSize) {
+          if ((i / pixelSize + j / pixelSize) % 2 === 0) {
+            layer.fill(200);
+          } else {
+            layer.fill(255);
+          }
+          layer.square(i, j, pixelSize);
         }
-        layer.square(i, j, pixelSize);
       }
-    }
-  };
-
-  const displayBackground2 = (layer) => {
-  let pixelSize = calculatePixelSize();
-  layer.background(255);
-  layer.strokeWeight(0);
-
-  for (let i = 0; i < layer.width; i += pixelSize) {
-    for (let j = 0; j < layer.height; j += pixelSize) {
-      const colIndex = Math.floor(i / pixelSize);
-      const rowIndex = Math.floor(j / pixelSize);
-      const color = p.color(156, 156, 156);
-
-      if (colIndex % 2 === 1 || rowIndex % 2 === 1) {
-        // Darken the color for intersecting lines
-        if (colIndex % 2 === 1 && rowIndex % 2 === 1) {
-          layer.fill(color.levels[0] - 50, color.levels[1] - 50, color.levels[2] - 50, 128);
-        } else {
-          layer.fill(color.levels[0], color.levels[0], color.levels[0], 128); // Gray
+    } else if (selectedBg === "background2") {
+      layer.clear();
+      for (let i = 0; i < layer.width; i += pixelSize) {
+        for (let j = 0; j < layer.height; j += pixelSize) {
+          const colIndex = Math.floor(i / pixelSize);
+          const rowIndex = Math.floor(j / pixelSize);
+          const color = p.color(200);
+      
+          if (colIndex % 2 === 1 || rowIndex % 2 === 1) {
+            // Darken the color for intersecting lines
+            if (colIndex % 2 === 1 && rowIndex % 2 === 1) {
+              layer.fill(color.levels[0] - 50, color.levels[1] - 50, color.levels[2] - 50, 128);
+            } else {
+              layer.fill(color.levels[0], color.levels[0], color.levels[0], 128); // Gray
+            }
+          } else {
+            layer.fill(255);
+          }
+      
+          layer.rect(i, j, pixelSize, pixelSize);
         }
-      } else {
-        layer.fill(255);
       }
+    } else if (selectedBg === "background3") {
+        const grayColor = layer.color(200);
+        const whiteColor = layer.color(255);
 
-      layer.square(i, j, pixelSize);
-    }
-  }
-};
+        for (let i = 0; i < layer.width; i += pixelSize) {
+          for (let j = 0; j < layer.height; j += pixelSize) {
+            const colIndex = Math.floor(i / pixelSize);
+            const rowIndex = Math.floor(j / pixelSize);
 
-const displayBackground3 = (layer) => {
-  let pixelSize = calculatePixelSize();
-  layer.background(255);
-  layer.strokeWeight(1);
+            if ((colIndex % 3 === 0 || colIndex % 3 === 1) && (rowIndex % 3 === 0 || rowIndex % 3 === 1)) {
+              // 2 blocks gray
+              layer.fill(grayColor);
+            } else if (colIndex % 3 === 2 && rowIndex % 3 === 2) {
+              // Intersection block white
+              layer.fill(whiteColor);
+            } else {
+              // 1 block white
+              layer.fill(whiteColor);
+            }
 
-  const grayColor = layer.color(156, 156, 156);
-  const whiteColor = layer.color(255);
-
-  for (let i = 0; i < layer.width; i += pixelSize) {
-    for (let j = 0; j < layer.height; j += pixelSize) {
-      const colIndex = Math.floor(i / pixelSize);
-      const rowIndex = Math.floor(j / pixelSize);
-
-      if ((colIndex % 3 === 0 || colIndex % 3 === 1) && (rowIndex % 3 === 0 || rowIndex % 3 === 1)) {
-        // 2 blocks gray
-        layer.fill(grayColor);
-      } else if (colIndex % 3 === 2 && rowIndex % 3 === 2) {
-        // Intersection block white
-        layer.fill(whiteColor);
-      } else {
-        // 1 block white
-        layer.fill(whiteColor);
+            layer.square(i, j, pixelSize);
+          }
+        }
+    } else if (selectedBg === "none") {
+      for (let i = 0; i < layer.width; i += pixelSize) {
+        for (let j = 0; j < layer.height; j += pixelSize) {
+          layer.stroke(0);
+          layer.strokeWeight(1);
+          layer.noFill();
+          layer.rect(i, j, pixelSize, pixelSize);
+        }
       }
-
-      layer.square(i, j, pixelSize);
     }
+
   }
-};
 
   const createButtonWithAction = (title, buttonPressedAction) => {
       let button = p.createButton(title);
@@ -154,15 +178,22 @@ const displayBackground3 = (layer) => {
   };
 
   const createSelectBg = () => {
-    let selectBg = p.createSelect();
+    selectBg = p.createSelect();
     selectBg.parent('btn-container');
 
-    selectBg.option('select bg');
-    selectBg.option('basic');
-    selectBg.option('plus-minus');
-    selectBg.option('grid');
+    selectBg.option('bg');
+    selectBg.option('basic', 'background1');
+    selectBg.option('+ -', 'background2');
+    selectBg.option('2:1','background3');
+    selectBg.option('none','none');
 
     selectBg.selected('select bg');
+    selectBg.changed(() => {
+      currentBackground = selectBg.value();
+      if (currentBackground !== 'select bg') {
+        showBackground(currentBackground);
+      }
+    });
   }
 
   const clearDrawing = () => {
@@ -170,18 +201,17 @@ const displayBackground3 = (layer) => {
       displayGrid(drawingLayer);
   };
 
-  const setTool = (label) => {
-    currentTool = label;
-    updateCursor();
+  const downloadImg = () => {
+    p.saveCanvas("kufix", "png");
   }
 
-  const updateCursor = () => {
-    if (currentTool === "Pen") {
+  const setTool = (tool) => {
+    currentTool = tool;
+    if (currentTool === "pen") {
       p.cursor(p.CROSS);
-    } else if (currentTool === "Eraser") {
+    } else if (currentTool === "eraser") {
       p.cursor(p.HAND);
     } else {
-      // Default cursor for other tools
       p.cursor(p.ARROW);
     }
   }
@@ -189,6 +219,28 @@ const displayBackground3 = (layer) => {
   const createColorPickers = () => {
     colorPicker = p.createColorPicker('#000000');
     colorPicker.parent('btn-container');
+  }
+
+  const createSliderPenSize = () => {
+    sliderPenSize = p.createSlider(1,5,1);
+    sliderPenSize.parent('btn-container');
+    sliderPenSize.size(60);
+    sliderPenSize.changed(() => {
+      penSize = sliderPenSize.value();
+    });
+  }
+
+  const toggleBackground = () => {
+    drawBackground = !drawBackground;
+  };
+
+  const createInputPixelLength = () => {
+    inputPixelLength = p.createInput(20, "number");
+    inputPixelLength.parent('btn-container');
+    inputPixelLength.changed(() => {
+      rowLength = inputPixelLength.value();
+      colLength = inputPixelLength.value();
+    })
   }
 
 };
